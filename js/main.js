8cb369a1a0b27b1cd43474d9e60f2aaeb54ff088
Christine <christine-xia@hotmail.com>
2020-03-30 02:01:46 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,22 @@ $('.main-title').hover(function(){
 })
 
 
+// HOVER OVER ACTUAL ARCHIVE
+
+$(document).mousemove(function(e){
+	$('.hoverimg').css({'top': e.clientY - 16 - 448, 'left': e.clientX + 16})
+})
+
+$('.actual').hover(
+	function(){
+		$('.hoverimg').css({'display':'block'})
+	},
+	function(){
+		$('.hoverimg').css({'display':'none'})
+	}
+)
+
+
 // COLLAPSIBLE BOXES https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
 
 var coll = document.getElementsByClassName("collapsible");
@@ -27,7 +43,7 @@ for (i = 0; i < coll.length; i++) {
 
 // POP UP WINDOW FOR RECORDS
 
-var windowAttr = 'height=620,width=950,left=425,top=167,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes';
+var windowAttr = 'height=650,width=970,left=421,top=151,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes';
 
 $('.records').mousedown(function(){
 	window.open('records/index.html','theRecordsInventory', windowAttr)
